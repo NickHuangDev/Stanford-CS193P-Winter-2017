1bf33040bc0090c13539a9de7fcb6818cb0eaa08
WormOnWheat <nick25932219@me.com>
2017-03-16 22:13:36 +0800

    [feature] Layout the calculator with storyboard

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var display: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - IBAction
+    @IBAction func touchDigit(_ sender: UIButton) {
+        
     }
-
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        
+    }
+    
 
 }
 
